bbcb442860b3e038dbc4c937e79b58372109f12f
Nasif Bidmus <nasif.bidmus@alight.com>
2024-06-18 14:10:08 -0500

    Remove data from OutreachRawData for Hilton

---
+++ DAT-5337/DataCleanUpRequest.sql
@@ -0,0 +1,11 @@
+-- select *
+--   FROM [Outreach].[dbo].[OutreachRawData]
+--   WHERE [DerivedEmployerID] = 5685
+--   ORDER BY [DateLoaded] DESC
+
+
+
+-- delete
+--   FROM [Outreach].[dbo].[OutreachRawData]
+--   WHERE [DerivedEmployerID] = 5685
+  
